817d47479b782c37934cf5706d210b337a13c829
Abacadae <mirko.wallach13@gmail.com>
2020-12-06 13:29:46 +0100

    Scripts and Test Objects

---
+++ VRTest2/Assets/Teleport.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public Transform teleportTarget;
+    public GameObject thePlayer;
+
+    public void teleport()
+    {
+        thePlayer.transform.position = teleportTarget.transform.position;
+    }
+}
